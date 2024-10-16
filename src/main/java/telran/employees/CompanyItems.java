a4cc5a073cab4e70b8312514b44df861e510afa4
yanfi-sher <loss.soul20@gmail.com>
2024-10-16 21:18:58 +0300

    init commit

---
+++ src/main/java/telran/employees/CompanyItems.java
@@ -0,0 +1,12 @@
+package telran.employees;
+
+import telran.view.Item;
+
+public class CompanyItems {
+    // TODO
+    public static Item[] getItems(Company company) {
+        // TODO
+        // item for each serverce method
+        throw new UnsupportedOperationException();
+    }
+}
